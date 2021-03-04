7dd5427fa2dbdb80af711fbee6b4a94a6caafab0
ivanCR56 <79504615+ivanCR56@users.noreply.github.com>
2021-03-03 18:23:38 -0600

    Add files via upload

---
+++ Intriducción.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+float a,b,sum;
+int main(void)
+
+    {
+        cout<< "\n ingrese el 1er numero\n";
+        cin>> a;
+        cout<<"\n ingrese el 2do numero\n";
+        cin>> b;
+        sum=a+b;
+        cout<<"\n La suma es ";
+        cout<< sum;
+        cout<< endl;
+        return 0;
+}
